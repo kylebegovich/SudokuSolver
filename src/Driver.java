d46f75c2ee7c4c38136f9d383a19d269ca40f8bb
Kyle Begovich <begovichkyle@gmail.com>
2017-01-03 15:05:52 -0600

    TODO list added to Driver.java for 2.0 release. 1.0 is middle-school level logic, 2.0 will be high-school plus level logic (should work for any sized puzzle given).

--- src/Driver.java
+++ src/Driver.java
@@ -20,4 +20,12 @@ public class Driver {
 		// does everything else
 		controller.run();
 	}
+	// TODO list
+	/* Work with rows, columns, and boxes as units, rather than just simple colving algorithms
+	 * Eliminate positions within a row, column, box to find what must go where
+	 * Partial positions regarding tuples of potential positions
+	 * 
+	 * Consider using a 3d array for the board to track each position's available array as well
+	 * 
+	 */
 }
