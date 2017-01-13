c85c0d617ea8ab8b9fd2d42b7e20803a58567203
Kyle Begovich <begovichkyle@gmail.com>
2017-01-13 15:25:03 -0600

    Added functionality to solve for structures in rows and columns in SophisticatedSolver.java without any isses left, boxes still to be worked through.

--- src/SophisticatedSolver.java
+++ src/SophisticatedSolver.java
@@ -8,7 +8,7 @@
  * Static class
  * ^Not sure if this will be maintained
  * 
- * @since January 11, 2017
+ * @since January 13, 2017
  * @author Kyle Begovich
  * @version 1.0
  */
@@ -30,18 +30,25 @@ public class SophisticatedSolver {
 	}
 
 	public static void solveRows(Model model) {
-	    // note that this is a rough draft and not necessarily
-        // reflective of the final version of this method
+	    // local versions of model's fields
         int length = model.getBoard().length;
         int[][] board = model.getBoard();
-        int counter = 0;
-        int[] standardArray = ArrayUtil.getStandardArray(length);
 
+        // used to record status of the board and it's properties
+        int counter;
+        int[] standardArray;
+
+        // for every row, check if any value can go in only one location, and if so, put it there
         for (int row = 0; row < length; row ++) {
+            // update variables used and modified
+            counter = 0;
+            standardArray = ArrayUtil.getStandardArray(length);
+
             int[] currentRow = board[row];
 
             for (int col = 0; col < length; col ++) {
                 if (currentRow[col] == 0) counter ++;
+                // TODO verify this line:
                 else standardArray[currentRow[col]-1] = 0;
             }
 
@@ -54,7 +61,6 @@ public class SophisticatedSolver {
                     counter ++;
                 }
             }
-            counter = 0;
 
             // at this point, neededNumbers[] is the set of numbers from
             // which every empty element in row[] == 0 needs to select one of
@@ -67,6 +73,7 @@ public class SophisticatedSolver {
                     numberAvailabilityArray[i][j] = -1;
                 }
             }
+
             // set any possible location as it's index value, instead of -1
             for (int i = 0; i < neededNumbers.length; i ++) {
                 for (int col = 0; col < length; col ++) {
@@ -99,72 +106,83 @@ public class SophisticatedSolver {
 	}
 	
 	public static void solveColumns(Model model) {
-	    // method currently commented out to prevent issues while testing solveRows()
-//        int length = model.getBoard().length;
-//        int[][] board = model.getBoard();
-//        int counter = 0;
-//        int[] standardArray = ArrayUtil.getStandardArray(length);
-//
-//        for (int col = 0; col < length; col ++) {
-//            // TODO correct this call to actually track the column instead of the row
-//            int[] currentColumn = board[col];
-//
-//            for (int row = 0; row < length; row ++) {
-//                if (currentColumn[row] == 0) counter ++;
-//                else standardArray[currentColumn[row]-1] = 0;
-//            }
-//
-//            int[] neededNumbers = new int[counter];
-//            counter = 0;
-//
-//            for (int i = 0; i < length; i ++) {
-//                if (standardArray[i] != 0) {
-//                    neededNumbers[counter] = standardArray[i];
-//                    counter ++;
-//                }
-//            }
-//            counter = 0;
-//
-//            // at this point, neededNumbers[] is the set of numbers from
-//            // which every empty element in row[] == 0 needs to select one of
-//
-//            // an array of all the numbers needed in the row and their respective possible locations
-//            int[][] numberAvailabilityArray = new int[neededNumbers.length][length];
-//            // initialize every spot to -1
-//            for (int i = 0; i < numberAvailabilityArray.length; i ++) {
-//                for (int j = 0; j < numberAvailabilityArray[0].length; j ++) {
-//                    numberAvailabilityArray[i][j] = -1;
-//                }
-//            }
-//            // set any possible location as it's index value, instead of -1
-//            for (int i = 0; i < neededNumbers.length; i ++) {
-//                for (int row = 0; row < length; row ++) {
-//                    if (ArrayUtil.canPlace(board, row, col, neededNumbers[i])) {
-//                        numberAvailabilityArray[row][i] = row;
-//                    }
-//                }
-//            }
-//
-//            // tracks where to put the number within the row
-//            int availableIndex = -1;
-//
-//            for (int i = 0; i < numberAvailabilityArray.length; i ++) {
-//                for (int j = 0; j < numberAvailabilityArray[0].length; j ++) {
-//                    if (numberAvailabilityArray[i][j] != -1) {
-//                        if (availableIndex == -1) {
-//                            availableIndex = numberAvailabilityArray[i][j];
-//                        } else {
-//                            availableIndex = -1;
-//                            break;
-//                        }
-//                    }
-//                }
-//                if (availableIndex != -1) {
-//                    board[availableIndex][col] = neededNumbers[i];
-//                    model.setBoard(board);
-//                }
-//            }
-//        }
+        // local versions of model's fields
+        int length = model.getBoard().length;
+        int[][] board = model.getBoard();
+
+
+
+        // for every column, check if any value can go in only one location, and if so, put it there
+        for (int col = 0; col < length; col ++) {
+            // used to record status of the board and it's properties
+            // update variables used and modified
+            int counter = 0;
+            int[] standardArray = ArrayUtil.getStandardArray(length);
+
+            // how to track a column as it's own entity
+            int[] currentColumn = new int[length];
+            for (int row = 0; row < length; row ++) {
+                currentColumn[row] = board[row][col];
+            }
+
+            // how to figure out how many number are needed
+            for (int row = 0; row < length; row ++) {
+                if (currentColumn[row] == 0) counter ++;
+                else standardArray[currentColumn[row]-1] = -1;
+            }
+
+            // resetting counter and setting up the array to hold the missing values
+            int[] neededNumbers = new int[counter];
+            counter = 0;
+
+            // filling the needed numbers array
+            for (int i = 0; i < length; i ++) {
+                if (standardArray[i] != -1) {
+                    neededNumbers[counter] = standardArray[i];
+                    counter ++;
+                }
+            }
+
+            // an array of all the numbers needed in the column and their respective possible locations
+            int[][] numberAvailabilityArray = new int[neededNumbers.length][length];
+
+            // initialize every spot to -1
+            for (int i = 0; i < numberAvailabilityArray.length; i ++) {
+                for (int j = 0; j < numberAvailabilityArray[0].length; j ++) {
+                    numberAvailabilityArray[i][j] = -1;
+                }
+            }
+
+            // set any possible location as it's index value, instead of -1
+            for (int i = 0; i < neededNumbers.length; i ++) {
+                for (int row = 0; row < length; row ++) {
+                    if (ArrayUtil.canPlace(board, row, col, neededNumbers[i])) {
+                        numberAvailabilityArray[i][row] = row;
+                    }
+                }
+            }
+
+            // tracks where to put the number within the column
+            int availableIndex = -1;
+
+            for (int i = 0; i < numberAvailabilityArray.length; i ++) {
+                for (int j = 0; j < numberAvailabilityArray[0].length; j ++) {
+                    if (numberAvailabilityArray[i][j] != -1) {
+                        if (availableIndex == -1) {
+                            availableIndex = numberAvailabilityArray[i][j];
+                        } else {
+                            // if there's more than one available index
+                            availableIndex = -1;
+                            break;
+                        }
+                    }
+                }
+                if (availableIndex != -1) {
+                    board[availableIndex][col] = neededNumbers[i];
+                    model.setBoard(board);
+                }
+            }
+        }
 	}
 	
 	public static void solveBoxes(Model model) {
