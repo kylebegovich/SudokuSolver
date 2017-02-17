560a864d2c4d20114e914a80366d3659523360bd
begovic2 <begovic2@illinois.edu>
2017-02-17 12:49:22 -0600

    Playing with Model.java, looks messy right now, wait a few days before looking

--- src/mvc/Model.java
+++ src/mvc/Model.java
@@ -9,15 +9,15 @@ import java.util.ArrayList;
 
 /**
  * The class that stores data and representations of the data
- * 
- * @since February 14, 2017
+ *
  * @author Kyle Begovich
  * @version 1.5
+ * @since February 14, 2017
  */
 public class Model {
-	private int[][] board;
-	private int[][][] complexBoard;
-	// TODO don't want to store these values, trying to deal with them live with an ArrayUtil method
+    private int[][] board;
+    private int[][][] complexBoard;
+    // TODO don't want to store these values, trying to deal with them live with an ArrayUtil method
     //private ArrayList<Integer> pairedPositions;
     //private Tuple pair;
 
@@ -25,9 +25,9 @@ public class Model {
     /**
      * Default constructor, not specifically important in this version
      */
-	public Model() {
-		super();
-	}
+    public Model() {
+        super();
+    }
 
 
     /**
@@ -35,20 +35,20 @@ public class Model {
      *
      * @return 2D int array representing the board
      */
-	public int[][] getBoard() {
-		return board;
-	}
+    public int[][] getBoard() {
+        return board;
+    }
 
     /**
      * Sets the instance field board to the input board
      *
      * @param board The board to store the value of
      */
-	public void setBoard(int[][] board) {
-		this.board = board;
-		complexBoard = new int[board.length][board.length][board.length];
-		updateComplexBoard();
-	}
+    public void setBoard(int[][] board) {
+        this.board = board;
+        complexBoard = new int[board.length][board.length][board.length];
+        updateComplexBoard();
+    }
 
 
     /**
@@ -56,14 +56,14 @@ public class Model {
      *
      * @return 3D int array representing the board and the available positions of
      */
-	public int[][][] getComplexBoard() {
-		return complexBoard;
-	}
+    public int[][][] getComplexBoard() {
+        return complexBoard;
+    }
 
-	// not sure if I will use this method yet, not sure when / where it would be applicable
-	public void setComplexBoard(int[][][] complexBoard) {
-		this.complexBoard = complexBoard;
-	}
+    // not sure if I will use this method yet, not sure when / where it would be applicable
+    public void setComplexBoard(int[][][] complexBoard) {
+        this.complexBoard = complexBoard;
+    }
 
 
     /**
@@ -79,91 +79,126 @@ public class Model {
     }
 
     // This is a mess at the moment
-	public int[] complexUpdateComplexBoard(int row, int col, int length) {
+    public int[] complexUpdateComplexBoard(int row, int col, int length) {
 
         int boxSize = (int) Math.sqrt(length);
-	    int rowStart = row - (row % boxSize);
-	    int colStart = col - (col % boxSize);
+        int rowStart = row - (row % boxSize);
+        int colStart = col - (col % boxSize);
 
-		// local instance of complexBoard;
+        // local instance of complexBoard;
         int[] available = complexBoard[row][col];
 
-		// length is the number of rows and columns that affect the box without intersecting (row, col)
-		int arrLength = (((int)Math.sqrt(length)) - 1) * 2;
+        // length is the number of rows and columns that affect the box without intersecting (row, col)
+        int arrLength = (((int) Math.sqrt(length)) - 1) * 2;
 
-		// an array of all the row and column indices to check for partial positions
-		int[] indexArray = new int[ arrLength ];
-		int counter = 0;
+        // an array of all the row and column indices to check for partial positions
+        int[] indexArray = new int[arrLength];
+        int counter = 0;
 
 
-		// first half of indexArray is row indices
-        for (int r = rowStart; r < boxSize + rowStart; r ++) {
+        // first half of indexArray is row indices
+        for (int r = rowStart; r < boxSize + rowStart; r++) {
             if (r != row) {
                 indexArray[counter] = r;
-				counter ++;
+                counter++;
             }
         }
 
         // second half of indexArray is col indices
-        for (int c = colStart; c < boxSize + colStart; c ++) {
+        for (int c = colStart; c < boxSize + colStart; c++) {
             if (c != col) {
                 indexArray[counter] = c;
-				counter ++;
+                counter++;
             }
         }
 
-		boolean needFirstPos = true;
+        boolean needFirstPos = true;
 
-        // checking rows
-		for (int r = 0; r < arrLength / 2; r ++) {
-			Tuple firstLocation = null;
-			for (int index = 0; index < board.length; index++) {
-				if (board[r][index] == 0) {
-					if (needFirstPos) {
-						firstLocation = new Tuple(r, index);
+        // checking rows for doubles
+        needFirstPos = checkRowsForPairedDoubles(arrLength, needFirstPos);
+
+        // checking columns for doubles
+        for (int c = arrLength / 2 + 1; c < arrLength; c++) {
+            Tuple firstLocation = null;
+            for (int index = 0; index < board.length; index++) {
+                if (board[index][c] == 0) {
+                    if (needFirstPos) {
+                        firstLocation = new Tuple(index, c);
                         needFirstPos = false;
-					} else {
-						Tuple secondPos = new Tuple(r, index);
+                    } else {
+                        Tuple secondPos = new Tuple(index, c);
 
-						ArrayList<Integer> pairedValues = ArrayUtil.getPairedPositions(firstLocation, secondPos, complexBoard);
-						if (pairedValues != null) {
-							ArrayUtil.dealWithPairedPositions(firstLocation, secondPos, pairedValues);
-							needFirstPos = true;
-						}
-					}
-				}
-			}
-		}
-
-		// checking columns
-		for (int c = arrLength / 2 + 1; c < arrLength; c++) {
-			Tuple firstLocation = null;
-			for (int index = 0; index < board.length; index++) {
-				if (board[index][c] == 0) {
-					if (needFirstPos) {
-						firstLocation = new Tuple(index, c);
-						needFirstPos = false;
-					} else {
-						Tuple secondPos = new Tuple(index, c);
-
-						ArrayList<Integer> pairedValues = ArrayUtil.getPairedPositions(firstLocation, secondPos, complexBoard);
+                        ArrayList<Integer> pairedValues = ArrayUtil.getPairedPositions(firstLocation, secondPos, complexBoard);
                         if (pairedValues != null) {
                             ArrayUtil.dealWithPairedPositions(firstLocation, secondPos, pairedValues);
                             needFirstPos = true;
                         }
-					}
-				}
-			}
-		}
-
-
-		// TODO check for triple paired positions in rows and columns
-
+                    }
+                }
+            }
+        }
 
         return available;
-	}
+    }
+
+    private boolean checkRowsForPairedDoubles(int arrLength, boolean needFirstPos) {
+        for (int r = 0; r < arrLength / 2; r++) {
+            Tuple firstLocation = null;
+            for (int index = 0; index < board.length; index++) {
+                if (board[r][index] == 0) {
+                    if (needFirstPos) {
+                        firstLocation = new Tuple(r, index);
+                        needFirstPos = false;
+                    } else {
+                        Tuple secondPos = new Tuple(r, index);
+
+                        ArrayList<Integer> pairedValues = ArrayUtil.getPairedPositions(firstLocation, secondPos, complexBoard);
+                        if (pairedValues != null) {
+                            ArrayUtil.dealWithPairedPositions(firstLocation, secondPos, pairedValues);
+                            needFirstPos = true;
+                        }
+                    }
+                }
+            }
+        }
+        return needFirstPos;
+    }
+
+
+
+
+
+
+
+
+
+
+    private boolean checkRowsForPairedPos(int arrLength, boolean needFirstPos) {
+        for (int r = 0; r < arrLength / 2; r++) {
+            Tuple firstLocation = null;
+            for (int index = 0; index < board.length; index++) {
+                if (board[r][index] == 0) {
+                    if (needFirstPos) {
+                        firstLocation = new Tuple(r, index);
+                        needFirstPos = false;
+                    } else {
+                        Tuple secondPos = new Tuple(r, index);
+
+                        ArrayList<Integer> pairedValues = ArrayUtil.getPairedPositions(firstLocation, secondPos, complexBoard);
+                        if (pairedValues != null) {
+                            ArrayUtil.dealWithPairedPositions(firstLocation, secondPos, pairedValues);
+                            needFirstPos = true;
+                        }
+                    }
+                }
+            }
+        }
+        return needFirstPos;
+    }
 
     // TODO write method similar to above, but for a set of 3 positions
+
+
 
 
     /**
@@ -182,19 +217,19 @@ public class Model {
      *
      * @return String, the puzzle, but pretty
      */
-	public String toString() {
-		String output = "";
-		int boxSize = (int) Math.sqrt(board.length);
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[0].length; j++) {
-				output += board[i][j] + " ";
-				if (i + 1 % boxSize == 0)
-					output += " ";
-			}
-			output += "\n";
-			if (i + 1 % boxSize == 0)
-				output += "\n";
-		}
-		return output;
-	}
+    public String toString() {
+        String output = "";
+        int boxSize = (int) Math.sqrt(board.length);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                output += board[i][j] + " ";
+                if (i + 1 % boxSize == 0)
+                    output += " ";
+            }
+            output += "\n";
+            if (i + 1 % boxSize == 0)
+                output += "\n";
+        }
+        return output;
+    }
 }
